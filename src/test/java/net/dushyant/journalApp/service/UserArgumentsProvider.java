6596f6ef89ab1e6a5e5e6668ba4e74e8f9540809
Dushy <dushyantverma.cs11@gmail.com>
2025-02-16 19:55:22 +0530

    Modified build.yml

--- src/test/java/net/dushyant/journalApp/service/UserArgumentsProvider.java
+++ src/test/java/net/dushyant/journalApp/service/UserArgumentsProvider.java
@@ -14,7 +14,7 @@ public class UserArgumentsProvider implements ArgumentsProvider {
     public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
         return Stream.of(
                 Arguments.of(User.builder().userName("shyam").password("shyam").build()),
-                Arguments.of(User.builder().userName("suraj").password("").build())
+                Arguments.of(User.builder().userName("suraj").password("suraj").build())
         );
     }
 }
